88af6c3fe3d7fa0edfb8053be16445ce9ead3183
Red <vhanchar@ucsc.edu>
2019-08-24 21:36:39 -0700

    added get request to fetch colors data

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,12 +1,36 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import axiosWithAuth from "../utilities/axiosWithAuth ";
+
+// When BubblePages renders, make a GET request to fetch the color data for your bubbles.
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+
   // fetch your colors data from the server when the component mounts
+  useEffect(() => {
+
+    const getColorsData = () => {
+
+        axiosWithAuth()
+
+        .get('http://localhost:5000/api/colors')
+
+        .then(response => {
+            console.log('response getdata: ', response.data);
+            setColorList(response.data);
+        })
+
+        .catch(error => {
+            console.log('error: ', error);
+        })
+    }
+    getColorsData();
+}, []);
+
+
   // set that data to the colorList state property
 
   return (
