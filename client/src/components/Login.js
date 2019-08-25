81aaed2a871da15d72a33317047a5f57d1cdd7d2
Red <vhanchar@ucsc.edu>
2019-08-24 20:55:46 -0700

    updated the login form with post request

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import axios from 'axios';
 
-const Login = () => {
+const Login = (props) => {
 
   // to store state
   const [credentials, setCredentials] = useState( { username: '', password: ''});
@@ -9,6 +9,23 @@ const Login = () => {
   // make a post request to retrieve a token from the api
   const login = event => {
     console.log('logging in');
+    event.preventDefault();
+    console.log('posting the following credentials: ', {credentials});
+    // 
+    axios
+    .post('http://localhost:5000/api/login', credentials)
+    .then(response => {
+        console.log('response value: ', response);
+        localStorage.setItem('token', response.data.payload);
+        console.log('props line 22: ', props);
+
+        // to redirect to list of friends
+        props.history.push('/friendslist');
+    })
+    .catch(error => {
+        console.log('error: ', error);
+    }
+    )
   }
 
   // to update the state when values change
@@ -26,6 +43,7 @@ const Login = () => {
       <h1> Login: </h1>
       <input type = 'text' name = 'username' placeholder = 'username' value = {credentials.username} onChange = {handleChange} />
       <input type = 'password' name = 'password' placeholder = 'password' value = {credentials.password} onChange = {handleChange} />
+      <button> log in </button>
     </form>
   );
 };
